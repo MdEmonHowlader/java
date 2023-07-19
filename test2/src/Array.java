8da222a5ba5fcc705f808080b170f44351eb51b4
Emon <emon22205101732@diu.edu.bd>
2023-07-19 23:31:21 +0600

    Array

---
+++ test2/src/Array.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class Array {
+    public static void main(String[] args) {
+        int[] marks={23,23,45,12};
+        int[][] finalMarks={{23,12,5,6},{29,65,48,35}};
+    for(int i=0; i<finalMarks.length; i++){
+        for (int j=0; j<finalMarks[i].length; j++){
+            System.out.println(finalMarks[i][j]);
+        }
+
+    }
+
+
+    }
+}
