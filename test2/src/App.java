02cfc66b5b773140f97c1774c6650b6f5f6cc1e7
Emon <emon22205101732@diu.edu.bd>
2023-08-01 14:51:00 +0600

    Average CGPA in java

--- test2/src/App.java
+++ test2/src/App.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! Emon");
+        double sum=0 ,number=0;
+        Scanner emon=new Scanner(System.in);
+       for(int i=1; i<5; i++){
+        System.out.println("Enter you number: ");
+       double cgpa=emon.nextDouble();
+        sum =sum+cgpa;
+       }
+        double Average=sum/5;
+       System.out.println("My Average: "+ Average );
+       if(Average>=80){
+        System.out.println("CGPA : 4");
+       }else if(75>=Average && Average>=70){
+        System.out.println("Cgpa: 3.75");
+       }else if(65>=Average && Average>=60){
+        System.out.println("Cgpa: 3.50");
+       }else if(55>=Average && Average>=50){
+        System.out.println("Cgpa: 3.25");
+       }else if(45>=Average && Average>=40){
+        System.out.println("Cgpa: 3.00");
+       }else{
+        System.out.println("Fall");
+       
+       }
     }
 }
---
+++ test_oop1/Test.java
@@ -0,0 +1,11 @@
+package test_oop1;
+
+public class Test {
+    int x=5;
+    int y=5;
+    int z=x+y;
+    public static void main(String[] args) {
+        Test myObj=new Test();
+        System.out.println(myObj.z);
+    }
+}
