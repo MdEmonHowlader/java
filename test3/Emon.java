ea4dfecb2441a3d5d8d44a3e2138caca21d05082
Emon <emon22205101732@diu.edu.bd>
2023-08-24 19:50:55 +0600

    update

--- test3/Emon.java
+++ test3/Emon.java
@@ -1,18 +1,57 @@
 package test3;
 
-import java.util.*;
+//import java.util.*;
 
 
-public class Emon {
-    public static void main(String[] args) {
-        int a=12;
-        int b=34;
-        double c=52.4;
+// public class Emon {
+//     public static void main(String[] args) {
+//         int a=12;
+//         int b=34;
+//         double c=52.4;
 
-        System.out.println(Math.max(a, b));
-        System.out.println(Math.min(a, b));
-        System.out.println(Math.sqrt(a));
+//         System.out.println(Math.max(a, b));
+//         System.out.println(Math.min(a, b));
+//         System.out.println(Math.sqrt(a));
         
-    }
+//     }
     
+// }
+
+ 
+import java.util.Scanner;
+ 
+public class Emon
+{
+ 
+public String name;
+ 
+private int marks;
+ 
+public Emon (String stuName) {
+name = stuName;
+}
+public void setMarks(int stuMar) {
+marks = stuMar;
+}
+ 
+// This method prints the student details.
+public void printStu() {
+System.out.println("Name: " + name );
+System.out.println("Marks:" + marks);
+}
+ 
+public static void main(String args[]) {
+Emon StuOne = new Emon("Emon1");
+Emon StuTwo = new Emon("Emon2");
+Emon StuThree = new Emon("Emon3");
+ 
+StuOne.setMarks(98);
+StuTwo.setMarks(89);
+StuThree.setMarks(90);
+ 
+StuOne.printStu();
+StuTwo.printStu();
+StuThree.printStu();
+ 
+}
 }
---
+++ test3/Project/LibraryManag.java
@@ -0,0 +1,24 @@
+package test3.Project;
+import java.util.*;
+
+public class LibraryManag {
+    public static void main(String[] args) {
+        Scanner myObj=new Scanner(System.in);
+       
+    System.out.println("Enter you Book Name: ");
+    String BookName=myObj.nextLine();
+     System.out.println("Enter you Author: ");
+    String Author=myObj.nextLine();
+     System.out.println("Enter you Book Genre: ");
+    String BookGenre=myObj.nextLine();
+     System.out.println("Enter you Book Id: ");
+    int BookId=myObj.nextInt();
+    
+    }
+
+    public String user(String BookName,String Author, String BookGenre, int BookId){
+
+    }
+
+}
+
