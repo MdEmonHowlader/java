110260ea42ebbd83054c2dd3e399b6d74cb93094
Emon <emon22205101732@diu.edu.bd>
2023-08-24 23:21:00 +0600

    update

--- test_oop1/Test.java
+++ test_oop1/Test.java
@@ -1,11 +1,10 @@
 package test_oop1;
 
 public class Test {
-    int x=5;
-    int y=5;
-    int z=x+y;
+    int x;
+   
     public static void main(String[] args) {
         Test myObj=new Test();
-        System.out.println(myObj.z);
+        System.out.println(myObj.x);
     }
 }
