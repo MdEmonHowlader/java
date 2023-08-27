446a60c69b9dca841c129b2f244a5727c1ba710b
Emon <emon22205101732@diu.edu.bd>
2023-08-27 14:52:26 +0600

    update

--- test4/Constr.java
+++ test4/Constr.java
@@ -2,6 +2,10 @@ package test4;
 //Constructor.....
 public class Constr {
     int x;
+
+    public Constr(){
+        int x=54;
+    }
     public static void main(String[] args) {
         Constr myObj=new Constr();
         System.out.println(myObj.x);
