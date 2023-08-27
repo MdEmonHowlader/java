5d3ca11c92aba2ba94167b01dab8bfa2ea4d47b3
Emon <emon22205101732@diu.edu.bd>
2023-08-27 18:58:13 +0600

    ArgumentMethode

--- test3/ArgumentMethode.java
+++ test3/ArgumentMethode.java
@@ -2,17 +2,25 @@ package test3;
 
 public class ArgumentMethode {
     void sum(int a, int b){
-       System.out.println(a+b);
+        a=45;
+        b=46;
+       System.out.println(a);
+       System.out.println(b);
         
     }
     void sum(double a, double b){
-      System.out.println(a+b);
+        a=45.76;
+        b=65.56;
+        System.out.println(a);
+        System.out.println(b);
 
     }
     public static void main(String[] args) {
         ArgumentMethode myObj= new ArgumentMethode();
-         myObj.sum(12, 45);
-         myObj.sum(12.45, 45.89);
+          myObj.sum(12, 45);
+         myObj.sum(12.5, 45.56);
+
+        
 
      
      
