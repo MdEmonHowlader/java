75fd33290284e859a039fb4440d7d28d2726400a
Emon <emon22205101732@diu.edu.bd>
2023-08-27 15:35:43 +0600

    Argument Sum

--- test3/ArgumentMethode.java
+++ test3/ArgumentMethode.java
@@ -2,13 +2,19 @@ package test3;
 
 public class ArgumentMethode {
     void sum(int a, int b){
+       System.out.println(a+b);
         
     }
     void sum(double a, double b){
+      System.out.println(a+b);
 
     }
     public static void main(String[] args) {
-        sum(10, 12);
-        sum(12.5, 56.7);
+        ArgumentMethode myObj= new ArgumentMethode();
+         myObj.sum(12, 45);
+         myObj.sum(12.45, 45.89);
+
+     
+     
     }
 }
