70ac9e84b18249181fdf2ae9d70ccd8bc5e5e46c
Emon <emon22205101732@diu.edu.bd>
2023-08-29 11:22:28 +0600

    normal array

--- test4/Array.java
+++ test4/Array.java
@@ -6,13 +6,9 @@ public class Array {
     public static void main(String[] args) {
         int[] num={2,3,6,5};
         String[] name={"Emon, limon, emon1"};
-        for(int i=0; i<num.length; i++){
-            System.out.println(num[i]);
-        }
-        for(int j=0; j<name.length; j++){
-            System.out.println(name[j]);
-        }
-        //System.out.println(Arrays.deepToString(name));
+       
+       System.out.println(Arrays.toString(num));
+        System.out.println(Arrays.deepToString(name));
         
     }
     
