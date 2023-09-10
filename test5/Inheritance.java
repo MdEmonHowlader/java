35be7eaafc0baa0c62e85e9d6bc5c832380a9959
Emon <emon22205101732@diu.edu.bd>
2023-09-10 23:34:05 +0600

    Inheritance

---
+++ test5/Inheritance.java
@@ -0,0 +1,6 @@
+package test5;
+
+public class Inheritance {
+    int salary=40000;
+    
+}
---
+++ test5/Programmer.java
@@ -0,0 +1,8 @@
+package test5;
+
+public class Programmer extends Inheritance {
+    public static void main(String[] args) {
+        Programmer p=new Programmer();
+        System.out.println(p.salary);
+        }
+}
