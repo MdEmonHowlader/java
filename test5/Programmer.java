cfa35079bed4c513a9f451d0460350434196a914
Emon <emon22205101732@diu.edu.bd>
2023-09-12 00:00:54 +0600

    update

--- test5/Programmer.java
+++ test5/Programmer.java
@@ -3,8 +3,9 @@ package test5;
 public class Programmer extends Inheritance {
     public static void main(String[] args) {
         Programmer p=new Programmer();
-        System.out.println("Salary: "+p.salary);
+        
         System.out.println("Name: "+p.name);
         System.out.println("Dep: "+p.Dep);
+        System.out.println("Salary: "+p.salary);
         }
 }
