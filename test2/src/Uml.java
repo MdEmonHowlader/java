34ca9318681d4d1902b455282d5f6914fbeda3b2
Emon <emon22205101732@diu.edu.bd>
2023-09-14 01:36:36 +0600

    update

--- test2/src/Uml.java
+++ test2/src/Uml.java
@@ -10,15 +10,18 @@ public class Uml {
     public static void main(String[] args) {
         Uml std1 = new Uml();
         std1.dept = "CSE";
-        std1.id = 1790;
+        std1.id = "23234";
         std1.cgpa = 4;
         System.out.println("Dept = " +std1.dept);
         System.out.println("id = " +std1.id);
         System.out.println("Cgpa = " +std1.cgpa);
         Uml std2 = new Uml();
         std2.dept = "English";
-        std2.id =1777;
+        std2.id ="324";
         std2.cgpa = 3;
+         System.out.println("Dept = " +std2.dept);
+        System.out.println("id = " +std2.id);
+        System.out.println("Cgpa = " +std2.cgpa);
     }
    
     
