5134ce6f1a3adebd4f137a92afe5a910e6b6267e
Emon <emon22205101732@diu.edu.bd>
2023-09-13 22:58:43 +0600

    Method11

---
+++ test2/src/Method111.java
@@ -0,0 +1,23 @@
+public class Method111 {
+    static void myName(){
+        String na="Emon";
+        System.out.println("Name: "+ na);
+           int id=1732;
+        System.out.println("ID: "+ id);
+           int sellar=17000;
+        System.out.println("Sellar: "+ sellar);
+           int post=173;
+        System.out.println("Post: "+ post);
+}
+static void myId(){
+        int id=17321;
+        System.out.println("ID: "+ id);
+}
+public static void main(String[] args) {
+
+        myName();
+        myId();
+        
+}
+    
+}
--- test4/AllCodeTest.java
+++ test4/AllCodeTest.java
@@ -2,9 +2,8 @@ package test4;
 
 
 public class AllCodeTest{
-        public static void main(String[] args) {
-                System.out.println("Emon");
-        }
+
+   
   
 }
                
