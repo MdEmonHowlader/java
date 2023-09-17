846b7ef17492d43eb7aa98a74d2df721fcc9fde7
Emon <emon22205101732@diu.edu.bd>
2023-09-17 13:46:38 +0600

    update

---
+++ test6/Animal.java
@@ -0,0 +1,20 @@
+package test6;
+import java.util.*;
+
+public class Animal {
+    private String name;
+
+    public void eat(){
+        
+
+    }
+    public void sleep(){
+
+    }
+    public void makeSound(){
+        System.out.println("Animal makes a generic sound");
+    }
+
+    
+    
+}
