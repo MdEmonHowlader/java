a60450614fae2de9a385417a8341a529fee70fb0
Emon <emon22205101732@diu.edu.bd>
2023-09-26 01:50:50 +0600

    update

---
+++ test8/Emon.java
@@ -0,0 +1,10 @@
+package test8;
+
+public class Emon extends Emon1{
+    public static void main(String[] args) {
+       Emon sc= new Emon();
+       System.out.println("Name:"+ sc.name);
+       sc.test();
+    }
+
+}
---
+++ test8/Emon1.java
@@ -0,0 +1,10 @@
+package test8;
+
+public class Emon1 {
+    protected String name= "Emon";
+
+  public void test(){
+    System.out.println("Hi Emon");
+  }
+    
+}
